8f22c29f400e6dff396e77ab0d1c0703a79cabd7
HarshTarkar <48978717+HarshTarkar@users.noreply.github.com>
2019-08-23 22:02:18 +0530

    Delete server.js

--- commands/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'server',
-	description: 'Rika sends the server name',
-	execute(message, args) {
-		message.channel.send("Welcome to the server! This server's name is " + message.guild.name);
-	},
-};
